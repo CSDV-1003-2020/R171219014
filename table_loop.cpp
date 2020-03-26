5d44a828c32037ee2869fe84df6760b683b87d79
nakul23721 <nakulverma2312@gmail.com>
2020-03-26 18:43:13 +0530

    properly arranged

--- table_loop.cpp
+++ table_loop.cpp
@@ -8,10 +8,15 @@ using namespace std;
 int main()
 {
     int n;
+   
     cin >> n;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    for(int i=1;i<11;i++)
-    cout<<n<<" x "<<i<<" = "<<n*i<<"\n";
+    
+   cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    
+   for(int i=1;i<11;i++)
+   
+   cout<<n<<" x "<<i<<" = "<<n*i<<"\n";
+    
     return 0;
 }
 
