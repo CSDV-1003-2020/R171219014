50db8be4201abd013130c217fdd810fe8e31ff66
sekhar76405 <ssjio5402@gmail.com>
2020-03-26 11:25:21 +0530

    (HACKERRANK Prob.) to print a table of given input

---
+++ table_loop.cpp
@@ -0,0 +1,17 @@
+//HAcker rank 30 days of code problem: - to print the table of the number inputed.
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    for(int i=1;i<11;i++)
+    cout<<n<<" x "<<i<<" = "<<n*i<<"\n";
+    return 0;
+}
+
