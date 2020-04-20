0de0d9dfaf3f9e134a3beb7c1913c9887d5f2dfa
nakul23721 <nakulverma2312@gmail.com>
2020-04-20 19:40:24 +0530

    Added Insert Function

--- stack_array.c
+++ stack_array.c
@@ -2,19 +2,39 @@
 int stack[6],top=-1,item=0;
 main()
 {
-  int a; 
-  while(1)
-  {  
-  printf("\npress \n 1 for insertion\n 2 for deletion \n 3 to exit ");
-  scanf("%d",&a);
+	  int a; 
+	    while(1)
+		      {  
+			        printf("\npress \n 1 for insertion\n 2 for deletion \n 3 to exit ");
+				  scanf("%d",&a);
 
-  switch(a)
-  {
-    case 1: insert(); break;
-	case 2: delete(); break;
-	case 3: exit(0);
-	default : printf("INVALID INPUT");  
-  }
-  }
+				    switch(a)
+					      {
+						          case 1: insert(); break;
+								  	case 2: delete(); break;
+											case 3: exit(0);
+													default : printf("INVALID INPUT");  
+														    }
+				      }
 }  
+
+insert()
+{
+	  if(top==5)
+		    printf("STACK IS FULL ");
+	    else
+		      {
+			          printf("enter the the value to be inserted :");
+				      scanf("%d",&item);
+				          top=top+1;
+					  	stack[top]=item;
+							printf("the entered value : %d has been inserted succesfully",item);
+							  }
+	      
+
+}
+
+
+
+
 
